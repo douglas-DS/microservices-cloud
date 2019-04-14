19eea928c99d6fe27414b6404b25207243bd0c8d
Douglas Souza <douglas_souza03@hotmail.com>
2019-04-14 17:26:42 -0300

    hystrix configuration

--- customer/src/main/java/br/com/ds/customer/CustomerController.java
+++ customer/src/main/java/br/com/ds/customer/CustomerController.java
@@ -1,5 +1,6 @@
 package br.com.ds.customer;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 
     @GetMapping
+    @HystrixCommand
     public Customer getCustomer() {
         return new Customer("Bruce Wayne", "BatCave", "PORT: 8080");
     }
